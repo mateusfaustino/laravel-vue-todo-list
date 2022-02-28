530c3d5d90802e72705d487754b65276215d694c
root <root@DESKTOP-324IIL8.localdomain>
2022-02-27 23:25:30 -0300

    vue and resr

--- routes/api.php
+++ routes/api.php
@@ -22,4 +22,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/items',[ItemController::class,'index']);
 Route::prefix('/item')->group(function(){
     Route::post('/',[ItemController::class,'store']);
+    Route::put('/{id}',[ItemController::class,'update']);
+    Route::delete('/{id}',[ItemController::class,'destroy']);
 });
